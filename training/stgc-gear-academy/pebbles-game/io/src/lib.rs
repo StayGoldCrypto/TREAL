fbf28ee453f596dcf16726cdf344065c4c0d81f7
StayGoldCrypto <106858653+StayGoldCrypto@users.noreply.github.com>
2024-08-12 20:34:42 -0300

    Update lib.rs
    cpy

--- training/stgc-gear-academy/pebbles-game/io/src/lib.rs
+++ training/stgc-gear-academy/pebbles-game/io/src/lib.rs
@@ -11,3 +11,46 @@ pub enum DifficultyLevel {
     Easy,
     Hard,
 }
+
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub enum PebblesAction {
+    Turn(u32),
+    GiveUp,
+    Restart {
+        difficulty: DifficultyLevel,
+        pebbles_count: u32,
+        max_pebbles_per_turn: u32,
+    },
+}
+
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub enum PebblesEvent {
+    CounterTurn(u32),
+    Won(Player),
+}
+
+#[derive(Debug, Default, Clone, Encode, Decode, TypeInfo)]
+pub enum Player {
+    #[default]
+    User,
+    Program,
+}
+
+#[derive(Debug, Default, Clone, Encode, Decode, TypeInfo)]
+pub struct GameState {
+    pub pebbles_count: u32,
+    pub max_pebbles_per_turn: u32,
+    pub pebbles_remaining: u32,
+    pub difficulty: DifficultyLevel,
+    pub first_player: Player,
+    pub winner: Option<Player>,
+}
+
+impl Metadata for PebblesMetadata {
+    type Init = In<PebblesInit>;
+    type Handle = InOut<PebblesAction, PebblesEvent>;
+    type State = Out<GameState>;
+    type Reply = ();
+    type Others = ();
+    type Signal = ();
+}

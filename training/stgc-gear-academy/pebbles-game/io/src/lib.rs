d17818c1aeacefa461ea6646bf4fe7e9b4431db5
StayGoldCrypto <106858653+StayGoldCrypto@users.noreply.github.com>
2024-08-12 16:23:19 -0300

    Update lib.rs
    Cpy

--- training/stgc-gear-academy/pebbles-game/io/src/lib.rs
+++ training/stgc-gear-academy/pebbles-game/io/src/lib.rs
@@ -1,1 +1,13 @@
+#[derive(Debug, Default, Clone, Encode, Decode, TypeInfo)]
+pub struct PebblesInit {
+    pub difficulty: DifficultyLevel,
+    pub pebbles_count: u32,
+    pub max_pebbles_per_turn: u32,
+}
 
+#[derive(Debug, Default, Clone, Encode, Decode, TypeInfo)]
+pub enum DifficultyLevel {
+    #[default]
+    Easy,
+    Hard,
+}

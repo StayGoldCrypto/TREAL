7827443f94198fe5709447f71ebb11f84cbf76f0
StayGoldCrypto <106858653+StayGoldCrypto@users.noreply.github.com>
2024-08-13 04:19:28 -0300

    Create template.rs
    init

---
+++ training/stgc-gear-academy/pebbles-game/src/template.rs
@@ -0,0 +1,16 @@
+#![no_std]
+use gstd::{};
+use pebbles_game_io: :*;
+static mut PEBBLES_GAME: 0ption<Gamestate> = None;
+#[no_mangle]
+extern "C" fn init(){
+    // YOUR CODE HERE
+}
+#[no_mangle]
+extern "C" fn handle(){
+    // YOUR CODE HERE
+}
+#[no_mangle]
+extern "C" fn state(){
+    // YOUR CODE HERE
+}

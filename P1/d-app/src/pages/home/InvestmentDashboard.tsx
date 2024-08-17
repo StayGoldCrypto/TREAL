c0356a21ce2b3ec791fee417ead8847440392621
StayGoldCrypto <106858653+StayGoldCrypto@users.noreply.github.com>
2024-08-16 22:21:25 -0300

    MVP Demo Partial Delivery v0.0.0
    This commit contains an operational react app that only shows basic demo functionality about TREAL, is not a functional finished minimal product, this is for demo only to demonstrate programming skills with React, TypeScript, NodeJS, Vitejs and GearJs protocol. In future releases, we will go deep into contract interactions.

---
+++ P1/d-app/src/pages/home/InvestmentDashboard.tsx
@@ -0,0 +1,13 @@
+// src/InvestmentDashboard.tsx
+import React from 'react';
+
+const InvestmentDashboard = () => {
+  return (
+    <div>
+      <h2>Investment Dashboard</h2>
+      <p>Here you can track your investments.</p>
+    </div>
+  );
+};
+
+export default InvestmentDashboard;

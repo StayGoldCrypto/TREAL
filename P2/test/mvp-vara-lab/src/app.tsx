1c35dae2ca9e3af8ac39dbe8155791db712f4648
StayGoldCrypto.com <sabrina@staygoldcrypto.com>
2024-09-01 03:48:28 +0000

    adding Vara-Lab Chakra UI

---
+++ P2/test/mvp-vara-lab/src/app.tsx
@@ -0,0 +1,26 @@
+import { useAccount, useApi } from "@gear-js/react-hooks";
+import { ApiLoader } from "@/components";
+import { Header } from "@/components/layout";
+import { withProviders } from "@/app/hocs";
+import { useWalletSync } from "@/features/wallet/hooks";
+import { Routing } from "./pages";
+import "@gear-js/vara-ui/dist/style.css";
+
+function Component() {
+  const { isApiReady } = useApi();
+  const { isAccountReady } = useAccount();
+
+  useWalletSync();
+
+  const isAppReady = isApiReady && isAccountReady;
+
+  // App with context
+  return (
+    <>
+      <Header isAccountVisible={isAccountReady} />
+      {isAppReady ? <Routing /> : <ApiLoader />}
+    </>
+  );
+}
+
+export const App = withProviders(Component);

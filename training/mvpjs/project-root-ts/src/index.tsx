d3f1e66e0d83a75cde37d1bb5fbbeaf18a8531a9
Alpha Team <106858653+StayGoldCrypto@users.noreply.github.com>
2024-09-03 02:38:02 -0300

    Update index.tsx

--- training/mvpjs/project-root-ts/src/index.tsx
+++ training/mvpjs/project-root-ts/src/index.tsx
@@ -1,5 +1,3 @@
-
-
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";   
@@ -21,45 +19,5 @@ ReactDOM.render(
 <React.StrictMode>
  <App />
 </React.StrictMode>,
-document.getElementById('App')
+document.getElementById('app')
 );
-
-
-/*
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <ChakraProvider theme={theme}>
-    <App />
-  </ChakraProvider>
-);
-*/
-
-/*
-
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const container = document.getElementById('app');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement); // Usa 'createRoot' en lugar de 'render'
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-//import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-*/

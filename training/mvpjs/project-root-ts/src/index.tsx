82398bf483fb7ade01e76d8238e10bd8681ac9b7
Alpha Team <106858653+StayGoldCrypto@users.noreply.github.com>
2024-09-03 02:22:04 -0300

    Update index.tsx
    chakra copy from main.tsx

--- training/mvpjs/project-root-ts/src/index.tsx
+++ training/mvpjs/project-root-ts/src/index.tsx
@@ -1,3 +1,30 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";   
+
+// Cargar variables de entorno desde el archivo .env
+dotenv.config();
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <ChakraProvider theme={theme}>
+    <App />
+  </ChakraProvider>
+);
+
+
+/*
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
@@ -6,7 +33,6 @@ const container = document.getElementById('app');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<App tab="home" />);
 
-/*
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';

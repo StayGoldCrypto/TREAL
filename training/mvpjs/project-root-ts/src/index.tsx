1bd597f63f7f9768215a72e263f76052d4c9fd73
Alpha Team <106858653+StayGoldCrypto@users.noreply.github.com>
2024-09-03 02:27:46 -0300

    Update index.tsx

--- training/mvpjs/project-root-ts/src/index.tsx
+++ training/mvpjs/project-root-ts/src/index.tsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";   
+import App from "./App";   
 
 // Cargar variables de entorno desde el archivo .env
 // dotenv.config();
@@ -14,7 +14,7 @@ const colors = {
   },
 };
 
-const theme = extendTheme({ colors });
+// const theme = extendTheme({ colors });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
  <App />

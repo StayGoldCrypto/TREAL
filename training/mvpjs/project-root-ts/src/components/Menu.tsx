38b2022d6b61816ba235068409dc33fedfe62bcc
Alpha Team <106858653+StayGoldCrypto@users.noreply.github.com>
2024-09-04 04:31:32 -0300

    Update Menu.tsx
    Dark Menu Sample

--- training/mvpjs/project-root-ts/src/components/Menu.tsx
+++ training/mvpjs/project-root-ts/src/components/Menu.tsx
@@ -18,9 +18,26 @@ const menuItems: MenuItem[] = [
     { label: 'Token', link: 'https://sabitashop.in/test/mvpjs/token/' },
 ];
 
+const MenuDark: React.FC = () => {
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand href="#">My Dark Website</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-dark" />
+            <Navbar.Collapse id="navbar-dark">
+                <Nav className="mr-auto">
+                    {menuItems.map((item, index) => (
+                        <Nav.Link key={index} href={item.link}>{item.label}</Nav.Link>
+                    ))}
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+/*
 const Menu: React.FC = () => {
     return (
-        <Navbar bg="dark" expand="lg">
+        <Navbar bg="light" expand="lg">
             <Navbar.Brand href="#">My Website</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
@@ -33,5 +50,71 @@ const Menu: React.FC = () => {
         </Navbar>
     );
 };
+
+
+const MenuLight: React.FC = () => {
+    return (
+        <Navbar bg="light" variant="light" expand="lg">
+            <Navbar.Brand href="#">My Light Website</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-light" />
+            <Navbar.Collapse id="navbar-light">
+                <Nav className="mr-auto">
+                    {menuItems.map((item, index) => (
+                        <Nav.Link key={index} href={item.link}>{item.label}</Nav.Link>
+                    ))}
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+const MenuPrimary: React.FC = () => {
+    return (
+        <Navbar bg="primary" variant="dark" expand="lg">
+            <Navbar.Brand href="#">My Primary Website</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-primary" />
+            <Navbar.Collapse id="navbar-primary">
+                <Nav className="mr-auto">
+                    {menuItems.map((item, index) => (
+                        <Nav.Link key={index} href={item.link}>{item.label}</Nav.Link>
+                    ))}
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+const MenuGradient: React.FC = () => {
+    return (
+        <Navbar expand="lg" style={{ background: 'linear-gradient(to right, #ff7e5f, #feb47b)' }}>
+            <Navbar.Brand href="#">Gradient Website</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-gradient" />
+            <Navbar.Collapse id="navbar-gradient">
+                <Nav className="mr-auto">
+                    {menuItems.map((item, index) => (
+                        <Nav.Link key={index} href={item.link} style={{ color: '#fff' }}>{item.label}</Nav.Link>
+                    ))}
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+const MenuTransparent: React.FC = () => {
+    return (
+        <Navbar expand="lg" style={{ background: 'transparent' }}>
+            <Navbar.Brand href="#">Transparent Website</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-transparent" />
+            <Navbar.Collapse id="navbar-transparent">
+                <Nav className="mr-auto">
+                    {menuItems.map((item, index) => (
+                        <Nav.Link key={index} href={item.link}>{item.label}</Nav.Link>
+                    ))}
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+*/
 
 export default Menu;
